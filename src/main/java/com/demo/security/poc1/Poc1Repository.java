119ebb26d7e2ce414d63742b05c91b7334a71ff5
Koos.Drost <kodrmsk@proton.me>
2026-05-13 12:10:48 +0200

    nieuwe cleane opzet voor alle onderdelen die gebruikt worden

--- src/main/java/com/demo/security/poc1/Poc1Repository.java
+++
@@ -1,6 +0,0 @@
-package com.demo.security.poc1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Geen zoekfunctie: AES-GCM is niet-deterministisch, zoeken is niet mogelijk (POC 3.1)
-public interface Poc1Repository extends JpaRepository<Poc1Entity, Long> {}
--- src/main/java/com/demo/security/poc4/Poc4Repository.java
+++
@@ -1,5 +0,0 @@
-package com.demo.security.poc4;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Poc4Repository extends JpaRepository<Poc4Entity, Long> {}
